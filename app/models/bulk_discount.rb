07fdf00d54451fa89abb25fe2f54e286ee232e51
Leigh <lcpulzone@gmail.com>
2021-06-14 10:42:08 -0700

    Add reltaionship

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,6 +2,7 @@ class BulkDiscount < ApplicationRecord
     belongs_to :merchant
     has_many :items, through: :merchant
     has_many :invoices, through: :merchant
+    has_many :invoice_items, through: :invoices
 
     validates :percentage, presence: true, numericality: true
     validates :quantity_threshold, presence: true, numericality: true
