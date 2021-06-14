b41bc27b202194a438b887a83f8405df6d3c845e
Leigh <lcpulzone@gmail.com>
2021-06-14 10:31:29 -0700

    Add relationships to models

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,7 @@
 class BulkDiscount < ApplicationRecord
     belongs_to :merchant
     has_many :items, through: :merchant
+    has_many :invoices, through: :merchant
 
     validates :percentage, presence: true, numericality: true
     validates :quantity_threshold, presence: true, numericality: true
