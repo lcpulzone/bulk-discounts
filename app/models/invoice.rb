6a3c0caef3c2fcc40a7d6b1bbbae65d90510ba94
Leigh <lcpulzone@gmail.com>
2021-06-14 19:24:49 -0700

    Add testing for discount links

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,6 +4,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   has_many :merchants, through: :items
+  has_many :bulk_discounts, through: :merchants
 
   enum status: { cancelled: 0, in_progress: 1, completed: 2}, _prefix: :status
 
@@ -29,10 +30,26 @@ class Invoice < ApplicationRecord
   end
 
   def merchant_total_revenue(merchant_id)
-    Invoice.joins(:items, :invoice_items).where('items.merchant_id = ?', merchant_id).where('invoices.id = ?', self.id).distinct.sum('invoice_items.unit_price * invoice_items.quantity')
+    Invoice.joins(:items, :invoice_items)
+    .where('items.merchant_id = ?', merchant_id)
+    .where('invoices.id = ?', self.id)
+    .distinct
+    .sum('invoice_items.unit_price * invoice_items.quantity')
   end
 
   def self.invoice_creation(item)
-    joins(:merchants, :items, :invoice_items).where('invoice_items.item_id = ?', item.id).where('merchants.id = ?', item.merchant_id).where('invoice_items.status = ?', 1).select('invoices.created_at').pluck(:created_at).strftime("%A, %B %d, %Y")
+    joins(:merchants, :items, :invoice_items)
+    .where('invoice_items.item_id = ?', item.id)
+    .where('merchants.id = ?', item.merchant_id)
+    .where('invoice_items.status = ?', 1).select('invoices.created_at')
+    .pluck(:created_at)
+    .strftime("%A, %B %d, %Y")
+  end
+
+  def invoice_revenue_with_discounts
+    price = invoice_items.map do |item|
+      item.total_item_sum
+    end
+    price.sum
   end
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,7 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   has_many :merchants, through: :item
+  has_many :bulk_discounts, through: :merchants
 
   validates :quantity, presence: true, numericality: true
   validates :unit_price, presence: true, numericality: true
@@ -19,5 +20,28 @@ class InvoiceItem < ApplicationRecord
 
   def self.invoice_id(item)
     joins(:merchants, :item, :invoice).where('invoice_items.item_id = ?', item.id).where('merchants.id = ?', item.merchant_id).where('invoice_items.status = ?', 1).select('invoice_items.invoice_id').pluck(:invoice_id).first
+  end
+
+  def best_discount
+    bulk_discounts.where('quantity_threshold <= ?', quantity)
+    .order(percentage: :desc)
+    .first
+  end
+
+  def invoice_item_total
+    (quantity * unit_price).round(2)
+  end
+
+  def discount_total_price
+    price = ((invoice_item_total * best_discount.turn_to_percent) - invoice_item_total).abs
+    price.round(2)
+  end
+
+  def total_item_sum
+    if best_discount.blank?
+      invoice_item_total
+    else
+      discount_total_price
+    end
   end
 end
