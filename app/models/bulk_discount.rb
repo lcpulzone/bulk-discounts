545edc31b19161d3b9c16e963cb3953b6a4520f1
Leigh <lcpulzone@gmail.com>
2021-06-14 17:53:45 -0700

    Add invoice_items relationship

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,9 +1,13 @@
 class BulkDiscount < ApplicationRecord
-    belongs_to :merchant
-    has_many :items, through: :merchant
-    has_many :invoices, through: :merchant
-    has_many :invoice_items, through: :invoices
+  belongs_to :merchant
+  has_many :items, through: :merchant
+  has_many :invoices, through: :merchant
+  has_many :invoice_items, through: :invoices
 
-    validates :percentage, presence: true, numericality: true
-    validates :quantity_threshold, presence: true, numericality: true
+  validates :percentage, presence: true, numericality: true
+  validates :quantity_threshold, presence: true, numericality: true
+
+  def turn_to_percent
+    ((percentage.to_f) / 100)
+  end
 end
